51d3249327ca25788683e13c2395e188755e5ba7
HASNAIN AHMED <113760859+HASNAIN005@users.noreply.github.com>
2023-09-28 02:32:14 -0700

    Add files via upload

---
+++ Lab4 Implement_lexical_analyzer_using_two_buffers/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Lab4_Implement_lexical_analyzer_using_two_buffers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBoxInput = new TextBox();
+            textBoxTokens = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            textBoxInput.Location = new Point(12, 129);
+            textBoxInput.Multiline = true;
+            textBoxInput.Name = "textBoxInput";
+            textBoxInput.Size = new Size(261, 127);
+            textBoxInput.TabIndex = 0;
+            // 
+            // textBoxTokens
+            // 
+            textBoxTokens.Location = new Point(529, 129);
+            textBoxTokens.Multiline = true;
+            textBoxTokens.Name = "textBoxTokens";
+            textBoxTokens.Size = new Size(259, 127);
+            textBoxTokens.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(357, 181);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBoxTokens);
+            Controls.Add(textBoxInput);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBoxInput;
+        private TextBox textBoxTokens;
+        private Button button1;
+    }
+}
